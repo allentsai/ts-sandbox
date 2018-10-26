57456cecf2e25b476f7d82e2247ef3ed95f61cd6
Allen Tsai <allentsai87@gmail.com>
2018-10-25 22:12:29 -0700

    Add ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {Test} from './test';
+import {readFileSync} from 'fs';
+import * as path from 'path';
 const test = new Test();
 test.getProperty();
 test.equals(1,1, "one still equals one!");
 test.equals(1,2);
 
+const file = readFileSync(path.resolve(__dirname, '../package.json'));
+console.log(JSON.parse(file.toString()));
